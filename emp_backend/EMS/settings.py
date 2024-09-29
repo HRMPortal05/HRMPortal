4e37a20b29970981a3ccc91c26547abff887265b
HRMPortal05 <hrmportal05@gmail.com>
2024-09-29 18:18:27 +0530

    Update settings.py

--- emp_backend/EMS/settings.py
+++ emp_backend/EMS/settings.py
@@ -143,7 +143,7 @@ DATABASES = {
 
 
 STATIC_URL= '/static/'
-if not DEBUG:
+if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
