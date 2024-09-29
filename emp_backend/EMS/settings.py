e298a1e7b4f3d0d79e022921ea916327db1fe0e3
HRMPortal05 <hrmportal05@gmail.com>
2024-09-29 18:13:26 +0530

    Update settings.py

--- emp_backend/EMS/settings.py
+++ emp_backend/EMS/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -203,7 +203,7 @@ REST_FRAMEWORK = {
 }
 
 SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = False
 
 # JWT Settings
