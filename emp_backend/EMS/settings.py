5694de38b2dd198f0e528adb2745a9971fb12af7
HRMPortal05 <hrmportal05@gmail.com>
2024-09-29 19:54:47 +0530

    Update settings.py

--- emp_backend/EMS/settings.py
+++ emp_backend/EMS/settings.py
@@ -37,7 +37,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'ems-igdr.onrender.com', 'hrm-portal-pfpl40r5t-hrm
 CSRF_TRUSTED_ORIGINS = [
     'https://ems-igdr.onrender.com',
     'https://hrm-portal-pfpl40r5t-hrmportal05s-projects.vercel.app',
-    'https://hrmportal.vercel.app/'
+    'https://hrmportal.vercel.app/',
+    'http://127.0.0.1:5173/'
 ]
 
 SESSION_COOKIE_SECURE = False
