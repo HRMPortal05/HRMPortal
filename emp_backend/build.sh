d917a0910c42eec9815f8c831b6ce0fd6e71c045
Tanishqvasani2906 <121330015+Tanishqvasani2906@users.noreply.github.com>
2024-09-29 17:53:52 +0530

    Update build.sh

--- emp_backend/build.sh
+++ emp_backend/build.sh
@@ -21,21 +21,5 @@ echo "Applying database migrations..."
 python manage.py migrate
 
 echo "Creating superuser..."
-python manage.py shell <<EOF > superuser_creation.log 2>&1
-from django.contrib.auth import get_user_model
-from django.core.exceptions import IntegrityError
-
-User = get_user_model()
-
-# Set superuser credentials
-superuser_name = 'administrator'
-superuser_email = 'hrmportal05@gmail.com'
-superuser_password = 'Hrmportal@005'
-
-try:
-    User.objects.create_superuser(superuser_name, superuser_email, superuser_password)
-except IntegrityError:
-    print("Superuser already exists. Skipping creation.")
-EOF
 
 echo "Build completed successfully."
