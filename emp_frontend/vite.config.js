6f8e41fd456240cfd4c75600431256e297b4b604
HRMPortal05 <hrmportal05@gmail.com>
2024-09-30 10:00:07 +0530

    Build

--- emp_frontend/vite.config.js
+++ emp_frontend/vite.config.js
@@ -6,6 +6,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "127.0.0.1", // Bind the server to 127.0.0.1
-    port: 5173, // Set the desired port
+    port: 5173, // Set the desired port for development
+  },
+  build: {
+    outDir: "dist", // Output directory for the build
+    rollupOptions: {
+      // Ensure that all assets are relative to the root directory
+      output: {
+        assetFileNames: "assets/[name].[hash].[ext]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
   },
 });
