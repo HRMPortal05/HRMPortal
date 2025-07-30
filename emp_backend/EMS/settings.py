e3f6f5109e3169c4b3124b2a672531c869b9188e
HRMPortal05 <hrmportal05@gmail.com>
2025-07-30 17:20:15 +0530

    Update settings.py

--- emp_backend/EMS/settings.py
+++ emp_backend/EMS/settings.py
@@ -32,12 +32,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ems-igdr.onrender.com', 'hrm-portal-pfpl40r5t-hrmportal05s-projects.vercel.app', 'hrmportal.vercel.app', 'hrm-portal-backend.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'ems-igdr.onrender.com', 'hrm-portal-pfpl40r5t-hrmportal05s-projects.vercel.app', 'hrmportal.vercel.app', 'hrmportal11.vercel.app', 'hrm-portal-backend.vercel.app']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://ems-igdr.onrender.com',
     'https://hrm-portal-pfpl40r5t-hrmportal05s-projects.vercel.app',
     'https://hrmportal.vercel.app',
+    'https://hrmportal11.vercel.app',
     'http://127.0.0.1:5173',
     'https://*.vercel.app'
 ]
@@ -51,6 +52,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://ems-igdr.onrender.com',
     'https://*.vercel.app',
     'https://hrmportal.vercel.app',
+    'https://hrmportal11.vercel.app',
     'https://hrm-portal-backend.vercel.app'
 ]
 
